18bde8c87bed01a1ae6269426b6a3bf394fedbd5
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-01-11 16:48:23 +0100

    Primer video

--- src/main/java/com/cristobalbernal/Socket/Cliente/EnviaTexto.java
+++ src/main/java/com/cristobalbernal/Socket/Cliente/EnviaTexto.java
@@ -13,7 +13,6 @@ public class EnviaTexto implements ActionListener {
             Socket miSockeet = new Socket("192.168.20.103",9999);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
-            System.out.println();
         }
     }
 }
