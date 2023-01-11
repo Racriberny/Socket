7bffb50904de0ea7e58c45df22c77636acbfed3a
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-01-11 19:31:03 +0100

    Segundo Video

--- src/main/java/com/cristobalbernal/Socket/Cliente/LaminaMarcoCliente.java
+++ src/main/java/com/cristobalbernal/Socket/Cliente/LaminaMarcoCliente.java
@@ -1,10 +1,14 @@
 package com.cristobalbernal.Socket.Cliente;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class LaminaMarcoCliente extends JPanel {
-    private JTextField campo1;
-    private JButton miBoton;
+
     public LaminaMarcoCliente(){
         JLabel texto = new JLabel("CLIENTE");
         add(texto);
@@ -15,4 +19,24 @@ public class LaminaMarcoCliente extends JPanel {
         miBoton.addActionListener(miEvento);
         add(miBoton);
     }
+    private JTextField campo1;
+    private JButton miBoton;
+
+    private class EnviaTexto implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                Socket miSockeet = new Socket("192.168.20.103",9999);
+                DataOutputStream flujo_salida = new DataOutputStream(miSockeet.getOutputStream());
+                flujo_salida.writeUTF(campo1.getText());
+                flujo_salida.close();
+
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+    }
 }
+
+
--- src/main/java/com/cristobalbernal/Socket/Servidor/MarcoServidor.java
+++ src/main/java/com/cristobalbernal/Socket/Servidor/MarcoServidor.java
@@ -2,8 +2,12 @@ package com.cristobalbernal.Socket.Servidor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class MarcoServidor extends JFrame {
+public class MarcoServidor extends JFrame implements Runnable{
     private JTextArea areaTexto;
     public MarcoServidor(){
         setBounds(1200,300,280,350);
@@ -13,5 +17,27 @@ public class MarcoServidor extends JFrame {
         milamina.add(areaTexto,BorderLayout.CENTER);
         add(milamina);
         setVisible(true);
+        Thread miHilo = new Thread(this);
+        miHilo.start();
+    }
+
+    @Override
+    public void run() {
+        //System.out.println("Estoy a la escucha");
+        try {
+            ServerSocket servidor = new ServerSocket(9999);
+
+            while (true){
+
+                Socket miSocket = servidor.accept();
+                DataInputStream flujo_entrada = new DataInputStream(miSocket.getInputStream());
+                String mensaje_texto = flujo_entrada.readUTF();
+                areaTexto.append("\n" + mensaje_texto);
+                miSocket.close();
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
